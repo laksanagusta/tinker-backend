e209f35e9a4fa61950038c757df099dac01f4411
dika <dikalaksana>
2020-12-20 12:00:21 +0800

    change app.js 3

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-var createError = require('http-errors');
 var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
   res.send("Dwadada")
+})
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log("helo")
 })
 
 
