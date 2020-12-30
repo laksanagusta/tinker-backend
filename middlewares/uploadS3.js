5cc1cdac3c0477f50d555e596915407589a2d6bc
dika <dikalaksana>
2020-12-30 10:06:44 +0800

    Pass delete photo if no photo

--- middlewares/uploadS3.js
+++ middlewares/uploadS3.js
@@ -59,64 +59,64 @@ const updateImage = (req, res, next) => {
       //console.table({err, fields, files})
       const {image} = files
 
-      if(image){
-          if (image.size > 2000000) {
-            return res.status(400).json({
-              error: "Image should be less than 2mb"
-            })
-          }
-
-          const deleteParams = {
-            Bucket: 'tinker-resource',
-            Key: `${fields.imagepath}`,
-          }
-
-          s3.deleteObject(deleteParams, function(err, data) {
-              if(err) return res.status(400).json({error: "Delete failed during update "+data})
-              else console.log('Delete during update', data)
-          })
-    
-          const params = {
-            Bucket: 'tinker-resource',
-            Key: `image/${uuidv4()}`,
-            Body: fs.readFileSync(image.path),
-            ACL: 'public-read',
-            ContentType: 'image/jpg'
-          }
-    
-          // Uploading files to the bucket
-          s3.upload(params, function(err, data) {
-            console.log(err)
-            if (err) res.status(400).json({error: "Upload to s3 failed"}) 
-            console.log('File upload success', data);
-            req.imagePath = data.key  
-            req.body = fields  
-            next()
-          });
-      }
-      else{
+      if(image === undefined){
         req.imagePath = data.key  
         req.body = fields  
+        next()
+      }
+      else{
+        if (image.size > 2000000) {
+          return res.status(400).json({
+            error: "Image should be less than 2mb"
+          })
+        }
+
+        const deleteParams = {
+          Bucket: 'tinker-resource',
+          Key: `${fields.imagepath}`,
+        }
+
+        s3.deleteObject(deleteParams, function(err, data) {
+            if(err) console.log("Delete failed during update")
+            else console.log('Delete during update', data)
+        })
+  
+        const params = {
+          Bucket: 'tinker-resource',
+          Key: `image/${uuidv4()}`,
+          Body: fs.readFileSync(image.path),
+          ACL: 'public-read',
+          ContentType: 'image/jpg'
+        }
+  
+        // Uploading files to the bucket
+        s3.upload(params, function(err, data) {
+          console.log(err)
+          if (err) res.status(400).json({error: "Upload to s3 failed"}) 
+          console.log('File upload success', data);
+          req.imagePath = data.key  
+          req.body = fields  
           next()
+        });
       }
     })
 }
 
 const deleteImage = (req, res, next) => {
-    const {imageUrl} = req.params
+  const {imageUrl} = req.params
 
-    const deleteParams = {
-        Bucket: 'tinker-resource',
-        Key: `image/${imageUrl}`,
-    }
+  const deleteParams = {
+      Bucket: 'tinker-resource',
+      Key: `image/${imageUrl}`,
+  }
 
-    s3.deleteObject(deleteParams, function(err, data) {
-        if(err) return res.status(400).json({error: "Delete failed during update "+data})
-        else console.log('Delete during update', data)
-    })
+  s3.deleteObject(deleteParams, function(err, data) {
+      if(err) return res.status(400).json({error: "Delete failed during update "+data})
+      else console.log('Delete during update', data)
+  })
 
-    req.body = req.params  
-    next()
+  req.body = req.params  
+  next()
 }
 
 
